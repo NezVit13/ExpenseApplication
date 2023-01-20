5cad94409f2469df9c1bc5a9f1df3c74f73a9865
nezvit13 <nezdojminoga@gmail.com>
2023-01-21 00:12:44 +0200

    created structure of app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Costs  from "./components/Costs";
 function App() {
+  const costs = [
+    {
+      date: new Date(2021, 2, 12),
+      description: 'Freezer',
+      amount: 999.99,
+    },
+        {
+      date: new Date(2021, 2, 2),
+      description: 'Freezer',
+      amount: 999.99,
+    },
+        {
+      date: new Date(2020, 1, 11),
+      description: 'Freezer',
+      amount: 999.99,
+    },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Costs  costs={costs} />
     </div>
   );
 }
